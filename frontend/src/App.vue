4d1a31b838b1f072b56fa6a773a196a269b13f5c
marblet <guitarmarble0117@gmail.com>
2020-06-18 13:29:22 +0900

    add header and details files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <v-app>
+    <Header />
     <v-content>
       <AreaList/>
     </v-content>
+    <v-footer>FindSafeCity</v-footer>
   </v-app>
 </template>
 
 <script>
 import AreaList from './components/AreaList';
+import Header from './components/Header';
 
 export default {
   name: 'App',
 
   components: {
     AreaList,
+    Header,
   },
 
   data: () => ({
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Find Safe City</p>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>

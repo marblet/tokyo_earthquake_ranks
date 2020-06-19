2fd84061c3bfd894113da496a57f3d84e08232d4
marblet <guitarmarble0117@gmail.com>
2020-06-19 10:41:14 +0900

    implement dialog (modal window)

--- frontend/src/components/AreaDetail.vue
+++ frontend/src/components/AreaDetail.vue
@@ -1,10 +1,17 @@
 <template>
-  
+  <v-card>
+    <v-card-title>{{ area.areaname }}</v-card-title>
+    <v-card-text>Dialog is called</v-card-text>
+  </v-card>
 </template>
 
 <script>
 export default {
-  
+  props: {
+    area: {
+      type: Object
+    }
+  }
 }
 </script>
 
--- frontend/src/components/AreaListItem.vue
+++ frontend/src/components/AreaListItem.vue
@@ -1,19 +1,33 @@
 <template>
-  <tr>
+  <tr v-on:click="dialog=true">
     <td>{{ area.areaname }}</td>
     <td>{{ area.collapse_rank}}</td>
     <td>{{ area.fire_rank}}</td>
     <td>{{ area.difficulty_rank}}</td>
     <td>{{ area.total_rank }}</td>
+    <v-dialog v-model="dialog">
+      <AreaDetail :area="area"></AreaDetail>
+    </v-dialog>
   </tr>
 </template>
 
 <script>
+import AreaDetail from './AreaDetail'
 export default {
+  components: {
+    AreaDetail
+  },
   props: {
     area: {
       type: Object
     }
+  },
+  data () {
+    return {
+      dialog: false
+    }
+  },
+  methods: {
   }
 }
 </script>

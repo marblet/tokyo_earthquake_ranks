c30b5d0da56c9e24acf70e97277454cf23c6edf1
marblet <guitarmarble0117@gmail.com>
2020-06-21 13:59:57 +0900

    refactor

--- frontend/src/components/AreaListItem.vue
+++ frontend/src/components/AreaListItem.vue
@@ -1,30 +1,18 @@
 <template>
-  <tr v-on:click="dialog=true">
-    <td>{{ area.areaname }}</td>
+  <tr>
+    <td>{{ area.municipality + area.town_name }}</td>
     <td>{{ area.collapse_rank}}</td>
     <td>{{ area.fire_rank}}</td>
     <td>{{ area.difficulty_rank}}</td>
     <td>{{ area.total_rank }}</td>
-    <v-dialog v-model="dialog">
-      <AreaDetail :area="area"></AreaDetail>
-    </v-dialog>
   </tr>
 </template>
 
 <script>
-import AreaDetail from './AreaDetail'
 export default {
-  components: {
-    AreaDetail
-  },
   props: {
     area: {
       type: Object
-    }
-  },
-  data () {
-    return {
-      dialog: false
     }
   }
 }

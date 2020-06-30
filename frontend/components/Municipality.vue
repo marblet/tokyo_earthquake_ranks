67f3a58fd49b885b4bef1dbbe9cc0772ab754ae3
marblet <guitarmarble0117@gmail.com>
2020-06-30 15:08:50 +0900

    update munic list

--- frontend/components/Municipality.vue
+++ frontend/components/Municipality.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="municipality-wrapper">
-    <div v-for="subarea in areaCodes" :key="subarea">
-      {{ subarea.name }}
-      <div v-for="area in subarea.list" :key="area">
-        <li>{{ areacode }}<router-link :to="`municipality/${area.area_code}`">{{ area.municipality }}</router-link></li>
-      </div>
+    <div v-for="subarea in areaCodes" :key="subarea.name" class="subarea-wrapper">
+      <h2>{{ subarea.name }}</h2>
+      <ul>
+        <li v-for="area in subarea.list" :key="area.area_code"><router-link :to="`municipality/${area.area_code}`">{{ area.municipality }}</router-link></li>
+      </ul>
      </div>
   </div>
 </template>
@@ -21,3 +21,24 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.municipality-wrapper {
+  padding: {
+    top: 10px;
+    bottom: 10px;
+    left: 10px;
+    right: 10px;
+  }
+  background: {
+    color: #FFF;
+  }
+}
+.subarea-wrapper ul {
+  overflow: hidden;
+}
+.subarea-wrapper li {
+  float: left;
+  width: 220px;
+}
+</style>

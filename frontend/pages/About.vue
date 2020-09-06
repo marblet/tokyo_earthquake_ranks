d4171d9991869a3316da67945577df8266c7f3c9
marblet <guitarmarble0117@gmail.com>
2020-09-06 16:48:28 +0900

    update descriptions

--- frontend/pages/About.vue
+++ frontend/pages/About.vue
@@ -3,22 +3,29 @@
     <h1>This is an about page</h1>
     <h2>サイトの説明</h2>
     <h3>建物倒壊</h3>
-    <p>{{ description.collapse.overview }}</p>
+    <CollapseDescription />
     <h3>火災</h3>
-    <p>{{ description.fire.overview }}</p>
+    <FireDescription />
     <h3>災害時</h3>
-    <p>{{ description.difficulty.overview }}</p>
+    <DifficultyDescription />
     <h3>総合</h3>
-    <p>{{ description.total.overview }}</p>
+    <p>建物倒壊危険度・火災危険度に災害時活動困難度を加味し、地震の揺れに起因する町丁目ごとの危険性を5段階にランク付けした。</p>
   </div>
 </template>
 
 <script>
 import Meta from '@/assets/mixins/Meta.js'
-import desc from '@/assets/jsons/description.json'
+import FireDescription from '@/components/FireDescription.vue'
+import CollapseDescription from '@/components/CollapseDescription.vue'
+import DifficultyDescription from '@/components/DifficultyDescription.vue'
 
 export default {
   name: 'About',
+  components: {
+    FireDescription,
+    CollapseDescription,
+    DifficultyDescription,
+  },
   mixins: [Meta],
   data () {
     return {
@@ -29,7 +36,6 @@ export default {
         url: 'https://example.com/test',
         // image: 'https://example.com/img/ogp/test.jpg',
       },
-      description: desc
     }
   }
 }
